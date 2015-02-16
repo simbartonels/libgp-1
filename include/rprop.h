5e6b2e191a1facf5f51da3afc2d2119b5effb59e
simbartonels <simbartonels@gmail.com>
2015-02-16 16:20:40 +0100

    refactored rprop implementation to record intermediate steps

--- include/rprop.h
+++ include/rprop.h
@@ -12,19 +12,35 @@ namespace libgp {
 
 /** Gradient-based optimizer.
  *  @author Manuel Blum */
-class RProp
-{
+class RProp {
 public:
-  RProp () {init();}
-  void init(double eps_stop = 0.0, double Delta0=0.1, double Deltamin=1e-6, double Deltamax=50, double etaminus=0.5, double etaplus=1.2);
-  void maximize(GaussianProcess * gp, size_t n=100, bool verbose=1);
+	RProp() {
+		init();
+	}
+	void init(double eps_stop = 0.0, double Delta0 = 0.1,
+			double Deltamin = 1e-6, double Deltamax = 50, double etaminus = 0.5,
+			double etaplus = 1.2);
+	void maximize(GaussianProcess * gp, size_t n = 100, bool verbose = 1);
+
+	/**
+	 * Maximizes the log-likelihood of the Gaussian process and returns the hyper-parameters found
+	 * in between.
+	 */
+	Eigen::MatrixXd maximize(GaussianProcess * gp, size_t n);
 private:
-  double Delta0;
-  double Deltamin;
-  double Deltamax;
-  double etaminus;
-  double etaplus;
-  double eps_stop;
+
+	/**
+	 * Performs an RProp step. Returns NAN if converged.
+	 */
+	double step(GaussianProcess * gp, double & best, Eigen::VectorXd & Delta,
+			Eigen::VectorXd & grad_old, Eigen::VectorXd & params,
+			Eigen::VectorXd & best_params);
+	double Delta0;
+	double Deltamin;
+	double Deltamax;
+	double etaminus;
+	double etaplus;
+	double eps_stop;
 };
 }
 
--- src/rprop.cc
+++ src/rprop.cc
@@ -32,9 +32,35 @@ void RProp::maximize(GaussianProcess * gp, size_t n, bool verbose)
   double best = log(0.0);
 
   for (size_t i=0; i<n; ++i) {
-    double lik = gp->log_likelihood();
-    if (verbose) std::cout << i << " " << -lik << std::endl;
-    Eigen::VectorXd grad = -gp->log_likelihood_gradient();
+	  double lik = step(gp, best, Delta, grad_old, params, best_params);
+	  if(lik == NAN)
+		  break;
+	  if (verbose) std::cout << i << " " << -lik << std::endl;
+  }
+  gp->covf().set_loghyper(best_params);
+}
+
+Eigen::MatrixXd RProp::maximize(GaussianProcess * gp, size_t n){
+	  int param_dim = gp->covf().get_param_dim();
+	  Eigen::MatrixXd param_history(n, param_dim);
+	  Eigen::VectorXd Delta = Eigen::VectorXd::Ones(param_dim) * Delta0;
+	  Eigen::VectorXd grad_old = Eigen::VectorXd::Zero(param_dim);
+	  Eigen::VectorXd params = gp->covf().get_loghyper();
+	  Eigen::VectorXd best_params = params;
+	  double best = log(0.0);
+
+	  for (size_t i=0; i<n; ++i){
+		  double lik = step(gp, best, Delta, grad_old, params, best_params);
+		  if(lik == NAN)
+			  break;
+		  param_history.row(i) = params;
+	  }
+	  gp->covf().set_loghyper(best_params);
+	  return param_history;
+}
+
+double RProp::step(GaussianProcess * gp, double & best, Eigen::VectorXd & Delta, Eigen::VectorXd & grad_old, Eigen::VectorXd & params, Eigen::VectorXd & best_params){
+	Eigen::VectorXd grad = -gp->log_likelihood_gradient();
     grad_old = grad_old.cwiseProduct(grad);
     for (int j=0; j<grad_old.size(); ++j) {
       if (grad_old(j) > 0) {
@@ -46,14 +72,14 @@ void RProp::maximize(GaussianProcess * gp, size_t n, bool verbose)
       params(j) += -Utils::sign(grad(j)) * Delta(j);
     }
     grad_old = grad;
-    if (grad_old.norm() < eps_stop) break;
+    if (grad_old.norm() < eps_stop) return NAN;
     gp->covf().set_loghyper(params);
+    double lik = gp->log_likelihood();
     if (lik > best) {
       best = lik;
       best_params = params;
     }
-  }
-  gp->covf().set_loghyper(best_params);
+    return lik;
 }
 
 }
