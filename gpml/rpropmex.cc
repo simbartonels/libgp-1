3e37256dcfec0311a0330c17e4a04dbaca33cb17
simbartonels <simbartonels@gmail.com>
2015-02-21 22:54:36 +0100

    changed the rprop gpml interface for faster evaluation

--- gpml/rpropmex.cc
+++ gpml/rpropmex.cc
@@ -2,6 +2,7 @@
 #include "gp.h"
 #include "gp_deg.h"
 #include "gp_fic.h"
+#include "gp_solin.h"
 #include "rprop.h"
 
 #include "mex.h"
@@ -10,6 +11,12 @@
 #include <Eigen/Dense>
 #include <iostream>
 #include <sstream>
+
+#define P_GP_NAME 5
+#define P_COV_NAME 6
+#define P_HYP 7
+#define P_M 8
+#define P_BF_NAME 9
 
 std::stringstream ss;
 
@@ -52,24 +59,24 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	char * gp_name_buf;
 	char * cov_name_buf;
 
-	if (nrhs < 7 || nlhs > 2) /* check the input */
+	if (nlhs < 5 || nrhs < 8) /* check the input */
 		mexErrMsgTxt(
-				"Usage: [hyp, theta_over_time] = rpropmex(seed, iters, X, y, gpName, covName, unwrap(hyp), M, bfName)");
+				"Usage: [times, theta_over_time, meanY, varY, nlZ, trainMean] = rpropmex(seed, iters, X, y, Xtest, gpName, covName, unwrap(hyp), M, bfName)");
 	seed = (size_t) mxGetScalar(prhs[0]);
 	iters = (size_t) mxGetScalar(prhs[1]);
 	n = mxGetM(prhs[2]);
 	D = mxGetN(prhs[2]);
 
-	buflen = getBufferLength(prhs, 4);
+	buflen = getBufferLength(prhs, P_GP_NAME);
 	gp_name_buf = (char *) mxCalloc(buflen, sizeof(char));
-	status = mxGetString(prhs[4], gp_name_buf, buflen);
+	status = mxGetString(prhs[P_GP_NAME], gp_name_buf, buflen);
 	if (!checkStatus(status, "GP name"))
 		return;
 	std::string gp_name(gp_name_buf);
 
-	buflen = getBufferLength(prhs, 5);
+	buflen = getBufferLength(prhs, P_COV_NAME);
 	cov_name_buf = (char *) mxCalloc(buflen, sizeof(char));
-	status = mxGetString(prhs[5], cov_name_buf, buflen);
+	status = mxGetString(prhs[P_COV_NAME], cov_name_buf, buflen);
 	if (!checkStatus(status, "Covariance function name"))
 		return;
 	std::string cov_name(cov_name_buf);
@@ -78,18 +85,18 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	} else {
 		if (nrhs < 9) {
 			mexErrMsgTxt(
-					"Usage: [hyp, theta_over_time] = rpropmex(seed, iters, X, y, gpName, covName, unwrap(hyp), M, bfName)");
+					"Usage: [times, theta_over_time, meanY, varY, nlZ, trainMean] = rpropmex(seed, iters, X, y, Xtest, gpName, covName, unwrap(hyp), M, bfName)");
 			return;
 		}
 		char * bf_name_buf;
-		size_t M = mxGetScalar(prhs[7]);
+		size_t M = mxGetScalar(prhs[P_M]);
 		if (M <= 0) {
 			mexErrMsgTxt("rpropmex: M must be greater 0!");
 			return;
 		}
-		buflen = getBufferLength(prhs, 8);
+		buflen = getBufferLength(prhs, P_BF_NAME);
 		bf_name_buf = (char *) mxCalloc(buflen, sizeof(char));
-		status = mxGetString(prhs[8], bf_name_buf, buflen);
+		status = mxGetString(prhs[P_BF_NAME], bf_name_buf, buflen);
 		if (!checkStatus(status, "Basis function name"))
 			return;
 		std::string bf_name(bf_name_buf);
@@ -97,10 +104,12 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 			gp = new libgp::DegGaussianProcess(D, cov_name, M, bf_name, seed);
 		} else if (gp_name.compare("FIC") == 0) {
 			gp = new libgp::FICGaussianProcess(D, cov_name, M, bf_name);
+		} else if (gp_name.compare("Solin") == 0) {
+			gp = new libgp::SolinGaussianProcess(D, cov_name, M, bf_name);
 		} else {
 			std::stringstream ss;
 			ss << "rpropmex: The GP name " << gp_name
-					<< " is unknown. Options are full, degenerate and FIC.";
+					<< " is unknown. Options are full, degenerate, Solin and FIC.";
 			mexErrMsgTxt(ss.str().c_str());
 			return;
 		}
@@ -114,7 +123,8 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	if (mxGetM(prhs[3]) != n) {
 		std::stringstream ss;
 		ss << "rpropmex: The number of training locations " << n
-				<< " does not match the number of training observations " << mxGetM(prhs[3]);
+				<< " does not match the number of training observations "
+				<< mxGetM(prhs[3]);
 		mexErrMsgTxt(ss.str().c_str());
 		return;
 	}
@@ -122,9 +132,21 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	for (size_t i = 0; i < n; i++)
 		gp->add_pattern(X.row(i), y(i));
 
-	p = mxGetM(prhs[6]);
-	Eigen::VectorXd params = Eigen::Map<const Eigen::VectorXd>(mxGetPr(prhs[6]),
-			p);
+	if (D != mxGetN(prhs[4])) {
+		std::stringstream ss;
+		ss << "rpropmex: The dimension of training locations " << D
+				<< " does not match the dimension of test locations "
+				<< mxGetN(prhs[4]);
+		mexErrMsgTxt(ss.str().c_str());
+		return;
+	}
+	size_t test_n = mxGetM(prhs[4]);
+	Eigen::MatrixXd testX = Eigen::Map<const Eigen::MatrixXd>(mxGetPr(prhs[4]),
+			test_n, D);
+
+	p = mxGetM(prhs[P_HYP]);
+	Eigen::VectorXd params = Eigen::Map<const Eigen::VectorXd>(
+			mxGetPr(prhs[P_HYP]), p);
 	if (p != gp->covf().get_param_dim()) {
 		std::stringstream ss;
 		ss << "The length of the initial parameter vector "
@@ -136,21 +158,39 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
 	}
 	mexPrintf("rpropmex: Initializating GP.\n");
 	gp->covf().set_loghyper(params);
-	mexPrintf("rpropmex: GP initialization complete. Starting hyper-parameter optimization.\n");
+	mexPrintf(
+			"rpropmex: GP initialization complete. Starting hyper-parameter optimization.\n");
 	libgp::RProp rprop;
-	if (nlhs == 2) {
-		plhs[1] = mxCreateDoubleMatrix(p + 1, iters, mxREAL);
-		Eigen::MatrixXd theta_over_time(p + 1, iters);
-		rprop.maximize(gp, theta_over_time);
-		Eigen::Map<Eigen::MatrixXd>(mxGetPr(plhs[1]), p + 1, iters) =
-				theta_over_time;
-	} else if (nlhs == 1) {
-		rprop.maximize(gp, iters, true);
-	}
-	plhs[0] = mxCreateDoubleMatrix(p, 1, mxREAL);
-	Eigen::Map<Eigen::VectorXd>(mxGetPr(plhs[0]), p) =
-			gp->covf().get_loghyper();
-	delete (gp);
+	Eigen::VectorXd times(iters);
+	Eigen::MatrixXd theta_over_time(p, iters);
+	Eigen::MatrixXd meanY(test_n, iters);
+	Eigen::MatrixXd varY(test_n, iters);
+	Eigen::VectorXd nlZ(iters);
+	Eigen::MatrixXd train_meanY(n, iters);
+	rprop.maximize(gp, testX, times, theta_over_time, meanY, varY, nlZ,
+			train_meanY);
+	std::cout << "rpropmex: results: " << std::endl;
+	std::cout << train_meanY.transpose() << std::endl;
+	plhs[0] = mxCreateDoubleMatrix(iters, 1, mxREAL);
+	Eigen::Map<Eigen::VectorXd>(mxGetPr(plhs[0]), iters) = times;
+	plhs[1] = mxCreateDoubleMatrix(p, iters, mxREAL);
+	Eigen::Map<Eigen::MatrixXd>(mxGetPr(plhs[1]), p, iters) = theta_over_time;
+	plhs[2] = mxCreateDoubleMatrix(test_n, iters, mxREAL);
+	Eigen::Map<Eigen::MatrixXd>(mxGetPr(plhs[2]), test_n, iters) = meanY;
+	plhs[3] = mxCreateDoubleMatrix(test_n, iters, mxREAL);
+	Eigen::Map<Eigen::MatrixXd>(mxGetPr(plhs[3]), test_n, iters) = varY;
+	plhs[4] = mxCreateDoubleMatrix(iters, 1, mxREAL);
+	Eigen::Map<Eigen::VectorXd>(mxGetPr(plhs[4]), iters) = nlZ;
+	plhs[5] = mxCreateDoubleMatrix(n, iters, mxREAL);
+	Eigen::Map<Eigen::MatrixXd>(mxGetPr(plhs[5]), n, iters) = train_meanY;
+//	if (nlhs >= 7) {
+//		plhs[6] = mxCreateDoubleMatrix(p, 1, mxREAL);
+//		Eigen::Map<Eigen::VectorXd>(mxGetPr(plhs[6]), p) =
+//				gp->covf().get_loghyper();
+//	}
+
+	//TODO: this is problem if gp=full. Why?
+//	delete (gp);
 	mexPrintf("Hyper-parameter optimization finished.");
 }
 
